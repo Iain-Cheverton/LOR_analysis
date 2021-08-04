9e2c3dc20213bbf59a58771b51e6c68ae0398923
Iain <lich294@gmail.com>
2021-08-04 16:07:00 +0100

    added plotting

--- tri_beam.py
+++ tri_beam.py
@@ -1,4 +1,8 @@
 """This file should be able to access card data and process it to show average outcomes at each mana cost"""
+import statistics as stats
+
+import matplotlib.pyplot as plt
+
 with open("follower_data.txt") as FILE:
     TEXT = FILE.read()
 # for line in TEXT.split("	"):
@@ -8,22 +12,40 @@ card_list = [card.split("\t") for card in CARDS]
 
 class Card:
     """temp"""
+
     def __init__(self, name, mana_cost, power, health):
         self.name = name
-        self.mana_cost = mana_cost
-        self.power = power
-        self.health = health
+        self.mana_cost = int(mana_cost)
+        self.power = int(power)
+        self.health = int(health)
+
+    def stat_total(self):
+        """The total stats for a card"""
+        return self.power + self.health
 
     def efficiency(self):
         """the total stats divided by the mana_cost"""
-        return (self.power + self.health) / self.mana_cost
+        return (self.power + self.health) / (self.mana_cost + 1)
 
 
+CARD_LIST = [Card(*follower) for follower in card_list]
 
-card_list_2 = [
-    Card(*follower[0], int(follower[1]), int(follower[2]), int(follower[3]))
-    for follower in card_list
-]
-for card in card_list_2:
-    if card.mana_cost == 1:
-        print(card.name)
+
+def average_result():
+    """Creates a dictionary of the average stat outcome for each possible mana cost of tri-beam"""
+    return {mana_cost: average(mana_cost) for mana_cost in range(1, 11)}
+
+
+def average(mana_cost):
+    """Finds the average stat value for a specific mana cost of follower"""
+    cards = [card for card in CARD_LIST if card.mana_cost == mana_cost]
+    return stats.mean(card.power for card in cards), stats.mean(
+        card.health for card in cards
+    )
+
+
+print(average_result().values())
+fig, ax = plt.subplots()
+ax.plot(average_result().keys(), [sum(y) for y in average_result().values()])
+ax.plot(average_result().keys(), average_result().values())
+plt.show()
