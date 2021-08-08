d2045fc7d7739ae7e366327e46981341c127cc4e
Iain <lich294@gmail.com>
2021-08-08 17:58:54 +0100

    changes after review

--- tri_beam.py
+++ tri_beam.py
@@ -1,12 +1,10 @@
-"""This file should be able to access card data and process it to show average outcomes at each mana cost"""
+"""
+Tri-Beam Improbulator is a card in legends of runeterra that can create a variety of different follower cards.
+This file plots a graph of the mana cost of these followers against the average stats they have.
+"""
 import statistics as stats
 
 import matplotlib.pyplot as plt  # pylint: disable=import-error
-
-with open("follower_data.txt") as FILE:
-    TEXT = FILE.read()
-CARDS = TEXT.split("\n")
-card_list = [card.split("\t") for card in CARDS]
 
 
 class Card:
@@ -39,25 +37,40 @@ class Card:
         return (self.power + self.health) / (self.mana_cost + 1)
 
 
-CARD_LIST = [Card(*follower) for follower in card_list]
-
-
-def average_result():
+def average_result(card_list):
     """Creates a dictionary of the average stat outcome for each possible mana cost of tri-beam"""
-    return {mana_cost: average(mana_cost) for mana_cost in range(1, 11)}
+    return {mana_cost: average(mana_cost, card_list) for mana_cost in range(1, 11)}
 
 
-def average(mana_cost):
+def average(mana_cost, card_list):
     """Finds the average stat value for a specific mana cost of follower"""
-    cards = [card for card in CARD_LIST if card.mana_cost == mana_cost]
+    cards = [card for card in card_list if card.mana_cost == mana_cost]
     return stats.mean(card.power for card in cards), stats.mean(
         card.health for card in cards
     )
 
 
-fig, ax = plt.subplots()
-ax.plot(average_result().keys(), [sum(y) for y in average_result().values()])
-ax.plot(average_result().keys(), average_result().values())
-plt.title("The Average Tri-beam Follower Stats For Each Possible Mana-Cost")
+def main():
+    """Shows a graph of the mana cost of the possible followers against the average stats they have"""
+    with open("follower_data.txt") as file:
+        card_list = [Card(*(line.split("\t"))) for line in file]
+    _, axis = plt.subplots()
+    axis.plot(
+        average_result(card_list).keys(),
+        [sum(y) for y in average_result(card_list).values()],
+        label="Total Stats",
+    )
+    axis.plot(
+        average_result(card_list).keys(),
+        average_result(card_list).values(),
+        label=("Power", "Health"),
+    )
+    axis.legend()
+    plt.title("The Average Tri-beam Follower Stats For Each Possible Mana-Cost")
+    plt.xlabel("Mana-Cost")
+    plt.ylabel("Stats")
+    plt.show()
 
-plt.show()
+
+if __name__ == "__main__":
+    main()
