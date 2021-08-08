202a65724532ccc0dcd69b06aeedb0aa3fbc8a6d
Iain <lich294@gmail.com>
2021-08-08 11:59:15 +0100

    added full docstrings

--- tri_beam.py
+++ tri_beam.py
@@ -1,17 +1,28 @@
 """This file should be able to access card data and process it to show average outcomes at each mana cost"""
 import statistics as stats
 
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt  # pylint: disable=import-error
 
 with open("follower_data.txt") as FILE:
     TEXT = FILE.read()
-# for line in TEXT.split("	"):
 CARDS = TEXT.split("\n")
 card_list = [card.split("\t") for card in CARDS]
 
 
 class Card:
-    """temp"""
+    """
+    A class used to represent a card
+     Attributes
+    ----------
+    name : str
+        the name of the Card
+    mana_cost : int
+        the mana required to play the card
+    power : int
+        the attack power of the card
+    health : int
+        the amount of damage the card can take before being destroyed
+    """
 
     def __init__(self, name, mana_cost, power, health):
         self.name = name
@@ -44,8 +55,9 @@ def average(mana_cost):
     )
 
 
-print(average_result().values())
 fig, ax = plt.subplots()
 ax.plot(average_result().keys(), [sum(y) for y in average_result().values()])
 ax.plot(average_result().keys(), average_result().values())
+plt.title("The Average Tri-beam Follower Stats For Each Possible Mana-Cost")
+
 plt.show()
